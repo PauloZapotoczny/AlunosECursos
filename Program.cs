e5e12b7ed37b89f3c32a347803b23ce9ae8d181a
PauloZapotoczny <paulorfz1122@gmail.com>
2024-06-01 14:03:45 -0300

    Commit do git

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Explorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Paulo", sobrenome: "Ricardo");
+Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Fernandes");
+
+
+Curso cursoDeIngles = new Curso(); 
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);   
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// p1.Idade = 18;
+
+// p1.Apresentar();
